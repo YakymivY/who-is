74e622574c583f82fc9ebc94b21aa783f2baffec
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2024-05-14 21:42:56 +0300

    category & personality added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './modules/main/main.component';
+import { CategoryComponent } from './modules/category/category.component';
+import { PersonalityComponent } from './modules/personality/personality.component';
 
 export const routes: Routes = [
-    { path: '', component: MainComponent }
+    { path: '', component: MainComponent },
+    { path: 'category:industry', component: CategoryComponent },
+    { path: 'personality', component: PersonalityComponent }
 ];
---
+++ src/app/modules/personality/personality.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personality',
+  standalone: true,
+  imports: [],
+  templateUrl: './personality.component.html',
+  styleUrl: './personality.component.css'
+})
+export class PersonalityComponent {
+
+}
